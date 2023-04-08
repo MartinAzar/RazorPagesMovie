e0a6989e52c6735476e99b15a62e45f2b3c58e22
Martin Azar <martin.azar@outlook.com>
2023-04-08 10:14:22 +1000

    Add Data Annotatiosn to Movie fields

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,13 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        
         public string? Title { get; set; }
-        [DataType(DataType.Date)] public DateTime ReleaseDate { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        
         public string? Genre { get; set; }
+
+        [Column(TypeName = "Decimal(18, 2")]
         public decimal Price { get; set; }
     }
 
